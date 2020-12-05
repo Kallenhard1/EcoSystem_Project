410f72a79ed55c98cee37817c3e030bcc709d603
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2020-12-05 11:46:59 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let ecoSystem;
+let ambient;
+let species;
+
+function setup() {
+  createCanvas(1920, 1080);
+  ecosystem = new EcoSystem();
+  ambient = new Ambient();
+  aspecies = new Species();
+  
+}
+
+function draw() {
+  background(220);
+}
