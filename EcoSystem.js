ec91cb4c8c168257b0f7901b7bd7d8ca13fa2356
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2020-12-05 11:48:42 -0300

    Create EcoSystem.js

---
+++ EcoSystem.js
@@ -0,0 +1,19 @@
+class EcoSystem {
+  constructor(x, y) {
+    this.grad = createVector(x, y);
+    this.time = time;
+    this.energy = energy;
+  }
+  
+  gradient() {
+    
+  }
+  
+  climate() {
+    
+  }
+  
+  energyFlow() {
+    
+  }
+}
