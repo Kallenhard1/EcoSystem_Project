4395e9287523abd03dbd56bdc162231f97c43f03
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2020-12-05 11:49:37 -0300

    Create Ambient.js

---
+++ Ambient.js
@@ -0,0 +1,15 @@
+class Ambient {
+  constructor(grad) {
+    this.lcurves = grad.copy();
+  }
+  
+  earth(grad, force, time) {
+    var earth = {
+      
+    };
+    var wather = {
+      
+    }
+  }
+  
+}
